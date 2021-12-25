b69675798fcc5239fc79c104b33073e618842704
Joker Ganteng <you@example.com>
2021-12-25 09:01:57 +0700

    Latihan Selenium Link Web : http://automationpractice.com/

---
+++ src/main/java/com/juaracoding/selenium/automationpractice/driver/DriverStrategyImplementator.java
@@ -0,0 +1,22 @@
+package com.juaracoding.selenium.automationpractice.driver;
+
+public class DriverStrategyImplementator {
+	
+	
+	public static DriverStrategy chooseStrategy(String strategy) {
+		
+		switch (strategy) {
+		case "chrome":
+			  return new Chrome();
+	
+		case "firefox":
+			 return new Firefox();
+        
+		default:
+			return null;
+		}
+		
+		
+	}
+
+}
